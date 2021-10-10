aa13cc364d0b80d3f177d2db3aaa53a50e36ad32
Brandon Shaw <unobrandon@mac.com>
2021-10-10 01:26:37 -0400

    added token generation

--- index.js
+++ index.js
@@ -1,3 +1,66 @@
+const {RtcTokenBuilder, RtmTokenBuilder, RtcRole, RtmRole} = require('agora-access-token')
+
+// Rtc Examples
+const appID = '404feedfd57c4ed2a3b7e3d5780c5114';
+const appCertificate = 'e60e21787d5d4b1aa3d03e9869b546cb';
+const channelName = 'testChannel';
+const uid = 2882341273;
+const account = "2882341273";
+const role = RtcRole.PUBLISHER;
+
+const expirationTimeInSeconds = 3600
+
+const currentTimestamp = Math.floor(Date.now() / 1000)
+
+const privilegeExpiredTs = currentTimestamp + expirationTimeInSeconds
+
+// IMPORTANT! Build token with either the uid or with the user account. Comment out the option you do not want to use below.
+
+// Build token with uid
+const tokenA = RtcTokenBuilder.buildTokenWithUid(appID, appCertificate, channelName, uid, role, privilegeExpiredTs);
+console.log("Token With Integer Number Uid: " + tokenA);
+
+// Build token with user account
+const tokenB = RtcTokenBuilder.buildTokenWithAccount(appID, appCertificate, channelName, account, role, privilegeExpiredTs);
+console.log("Token With UserAccount: " + tokenB);
+
+/*
+//WORKING CODE BELOW
+const https = require("https");
+
+https
+  .get("https://api.lil.software/stocks?symbol=AAPL", resp => {
+    let data = "";
+
+    resp.on("data", chunck => {
+      data += chunck;
+    });
+
+    resp.on("end", () => {
+      let url = JSON.parse(data);
+      console.log(url);
+    });
+  })
+  .on("error", err => {
+    console.log("Error: " + err.message);
+  });
+*/
+
+/*
+const express = require('express');
+const app = express();
+let port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hellow world");
+});
+
+app,listen(port, () => {
+  console.log('Example app is lostening on port http://localhost:$(port)');
+});
+*/
+
+/*
 const express = require('express')
 const path = require('path')
 const unirest = require('unirest');
@@ -26,3 +89,4 @@ app.get('/get_weather', function(req, resp) {
     resp.send(res.raw_body)
   });
 })
+*/
