85e4040a81c00d21b8358d3394b44c948c6b34f5
Brandon Shaw <unobrandon@mac.com>
2021-10-09 23:51:39 -0400

    installed something

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const path = require('path')
+const unirest = require('unirest');
+const PORT = process.env.PORT || 5000
+var bodyParser = require('body-parser');
+
+const app = express()
+
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(bodyParser.json());
+app.use(bodyParser.urlcoded({
+  extended: true
+}));
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.get('/', (req, res) => console.log('Listening on ${ PORT }'))
+
+app.get('/get_weather', function(req, resp) {
+  var unirest = require('unirest');
+  var req = unirest('GET', 'https://api.lil.software/weather?latitude=40.709335&longitude=-73.956558')
+  .end(function (res) {
+    if (res.error) throw new Error(res.error);
+    console.log(res.raw_body);
+    resp.send(res.raw_body)
+  });
+})
